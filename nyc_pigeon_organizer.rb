115701d5f1ce2887be259442abd372888fb9c28c
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 14:59:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,4 @@
 def nyc_pigeon_organizer(data)
   pigeon_data=Hash.new
-  pigeon_data=["Theo, Peter Jr., Lucky, Ms. K, Queenie, Andrew, and Alex"]
+ # pigeon_data=["Theo, Peter Jr., Lucky, Ms. K, Queenie, Andrew, and Alex"]
 end
